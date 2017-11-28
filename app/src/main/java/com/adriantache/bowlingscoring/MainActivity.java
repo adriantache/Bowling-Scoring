554a68dd9d3332d444e45dfde1b354ee205b5396
Adrian Tache <contact@adriantache.com>
2017-11-28 13:34:27 +0200

    added all TextView ids for scores

--- app/src/main/java/com/adriantache/bowlingscoring/MainActivity.java
+++ app/src/main/java/com/adriantache/bowlingscoring/MainActivity.java
@@ -2,6 +2,7 @@ package com.adriantache.bowlingscoring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // declaring the variable arrays that contain the scores
+    int[] frameScoresPlayer1 = new int[21];
+    int[] frameScoresPlayer2 = new int[21];
+    int[] frameTotalScoresPlayer1 = new int[10];
+    int[] frameTotalScoresPlayer2 = new int[10];
+
+    // this function updates all the TextViews that include scores, by assigning values from
+    // the vectors with frame scores and frame total scores
+    public void updateScores(View view){
+
+
+    }
+
+    //
+
+
 }
