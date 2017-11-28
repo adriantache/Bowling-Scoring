7ad0d4cceac1dd81990cdb432f150bb83be7814f
Adrian Tache <contact@adriantache.com>
2017-11-28 21:50:36 +0200

    Finish selection of number of downed pins

--- app/src/main/java/com/adriantache/bowlingscoring/MainActivity.java
+++ app/src/main/java/com/adriantache/bowlingscoring/MainActivity.java
@@ -10,23 +10,30 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     // declare the array that contains the images for number of downed pins
-    // and the pointer to the position in the array
-    int downedPinsPointer = 0;
-    int[] downedPins;
-    // declare the variable arrays that contain the scores
-    int[] frameScoresPlayer1 = new int[21];
-    int[] frameScoresPlayer2 = new int[21];
-    int[] frameTotalScoresPlayer1 = new int[10];
-    int[] frameTotalScoresPlayer2 = new int[10];
+    // and the pointer to the position in the array and the downedPins ImageView
+    // to prevent always searching for it.
+    // Thanks a lot to @Thor on Slack for the solution to the downedPins mechanism!
+    int downedPinsPointer = -1;
+    TypedArray downedPins;
+    ImageView imageViewdownedPins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get the array from arrays.xml
-        downedPins = getResources().getIntArray(R.array.number_of_pins);
+        // get the array from arrays.xml
+        downedPins = getResources().obtainTypedArray(R.array.number_of_pins);
+
+        // define the ImageView for downedPins to use later
+        imageViewdownedPins = findViewById(R.id.downedPins);
     }
+
+    // declare the variable arrays that contain the scores
+    int[] frameScoresPlayer1 = new int[21];
+    int[] frameScoresPlayer2 = new int[21];
+    int[] frameTotalScoresPlayer1 = new int[10];
+    int[] frameTotalScoresPlayer2 = new int[10];
 
     // update all the TextViews that display scores, by assigning values from
     // the vectors with frame scores and frame total scores (split/condition it per player?)
@@ -39,10 +46,15 @@ public class MainActivity extends AppCompatActivity {
         if (downedPinsPointer < 10) {
             downedPinsPointer++;
         }
-        /*Drawable drawable = downedPins.getDrawable(downedPinsPointer);*/
+
+        /*ImageView imageView = (ImageView) this.findViewById(R.id.downedPins);*/
+        imageViewdownedPins.setImageResource(downedPins.getResourceId(downedPinsPointer, -1) );
+
+        /*Drawable drawable = downedPins.getDrawable(downedPinsPointer);*//*
         ImageView imageView = (ImageView) this.findViewById(R.id.downedPins);
-        imageView.setImageResource(downedPins[downedPinsPointer]);
-        imageView.invalidate();
+        imageView downedPins.setImageResource(downedPins.getResourceId(downedPinsPointer, -1) );
+       *//* imageView.setImageResource(downedPins[downedPinsPointer]);
+        imageView.invalidate();*/
     }
 
     // decrement the drawable vector to show number of downed pins
@@ -50,10 +62,13 @@ public class MainActivity extends AppCompatActivity {
         if (downedPinsPointer > 0) {
             downedPinsPointer--;
         }
-        /*Drawable drawable = downedPins.getDrawable(downedPinsPointer);*/
+
+        imageViewdownedPins.setImageResource(downedPins.getResourceId(downedPinsPointer, -1) );
+
+       /* *//*Drawable drawable = downedPins.getDrawable(downedPinsPointer);*//*
         ImageView imageView = (ImageView) this.findViewById(R.id.downedPins);
         imageView.setImageResource(downedPins[downedPinsPointer]);
-        imageView.invalidate();
+        imageView.invalidate();*/
     }
 
     // process the indicated number of downed pins
